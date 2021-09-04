e1fd1987263463372e0633ab2d28a19e8f741f19
Priya Kothalkar <priyakothalkar2973@gmail.com>
2021-09-04 18:28:41 +0530

    refactor script

---
+++ script.js
@@ -0,0 +1,29 @@
+var initialPrice = document.querySelector('#initial-price');
+var quantity = document.querySelector('#quantity');
+var currentPrice = document.querySelector('#current-price');
+
+var button = document.querySelector('#button');
+var output = document.querySelector('#output');
+
+button.addEventListener('click', clickHandler);
+
+function clickHandler(){
+    var ip = initialPrice.value;
+    var q = quantity.value;
+    var cp = currentPrice.value;
+    calculate(ip, q, cp);
+}
+function calculate(iniPrice, quant, currPrice){
+    if(iniPrice > currPrice){
+        var loss = (iniPrice - currPrice) * quant;
+        var lossPercent = (loss / iniPrice) * 100;
+        output.innerText = `You are in Loss! The loss is ${loss} and loss percentage ${lossPercent}`;
+        // output.innerText =
+    }else if(currPrice > iniPrice){
+        var profit = (currPrice - iniPrice) * quant;
+        var profitPercent = (profit / iniPrice) * 100;
+        output.innerText = `you are in Profit! The Profit is ${profit} and profit percentage is ${profitPercent}.`;
+    }else{
+        output.innerText = 'No Pain No gain!';
+    }
+}
